308a87ba4562e3a4c7a93ff9cf7129ea93905c49
jennyinfinity <jenny.xu@ipacc.com>
2017-07-19 23:04:20 -0500

    Update image.php

--- image.php
+++ image.php
@@ -5,7 +5,7 @@ $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = "green";
 
-$color = imagecolorallocate($im, 0, 255, 0);
+$color = imagecolorallocate($im, 0, 210, 13);
 
 
 // Draw a filled rectangle
